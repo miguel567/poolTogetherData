f9d15f379fcbc74edc86f0c5c939c0aa0a8eb601
LAPTOP-BMG44977\migue <miguel567@gmail.com>
2020-02-24 23:07:51 +0100

    getting logs working

--- getAccounts.js
+++ getAccounts.js
@@ -1,6 +1,6 @@
 const Web3 = require ('web3');
 
-let run = () => {
+let run = async () => {
 
 var web3 = new Web3 ('wss://mainnet.infura.io/ws/v3/b842c5076ac4499884e201d7948dc9d2');
 /* console.log(eth); */
@@ -27,9 +27,13 @@ subscription.unsubscribe(function(error, success){
     if(success)
         console.log('Successfully unsubscribed!');
 }); */
+var options = {
+    fromBlock: 9523259,
+    toBlock: 'latest', 
+    address: ['0x29fe7D60DdF151E5b52e5FAB4f1325da6b2bD958']
 
-
-
+}
+ web3.eth.getPastLogs(options).then(console.log).catch(e => console.log(e));
 
 }
 
