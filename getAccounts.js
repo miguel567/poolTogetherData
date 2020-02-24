ec2205bf510b2f455cb4fa223be463d48e737e7e
LAPTOP-BMG44977\migue <miguel567@gmail.com>
2020-02-24 22:43:26 +0100

    working

--- getAccounts.js
+++ getAccounts.js
@@ -2,19 +2,11 @@ const Web3 = require ('web3');
 
 let run = () => {
 
-var web3 = new Web3 ('https://mainnet.infura.io/v3/b842c5076ac4499884e201d7948dc9d2');
+var web3 = new Web3 ('wss://mainnet.infura.io/ws/v3/b842c5076ac4499884e201d7948dc9d2');
 /* console.log(eth); */
-var filter = web3.eth.filter({toBlock:'latest'});
-
-filter.watch(function (error, log) {
- console.log(log); //  {"address":"0x0000000000000000000000000000000000000000", "data":"0x0000000000000000000000000000000000000000000000000000000000000000", ...}
-});
-
-// get all past logs again.
-var myResults = filter.get(function(error, logs){  });
-// stops and uninstalls the filter
-filter.stopWatching();
-
+web3.eth.getBlockNumber((err,block) => {
+    console.log(block);
+})
 
 
 
