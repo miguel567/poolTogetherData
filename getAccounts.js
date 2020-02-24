a00be8f2bf655c0d9e4082d711a2d1eb7ceb50c2
LAPTOP-BMG44977\migue <miguel567@gmail.com>
2020-02-24 22:39:36 +0100

    done

--- getAccounts.js
+++ getAccounts.js
@@ -3,7 +3,7 @@ const Web3 = require ('web3');
 let run = async () => {
 
 var web3 = new Web3 ('wss://mainnet.infura.io/ws/v3/b842c5076ac4499884e201d7948dc9d2');
-console.log(web3.currentProvider);
+
 
 var blockHeader = await web3.eth.getBlockNumber((err,block) => {
     console.log(block);
